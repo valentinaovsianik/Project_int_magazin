1c82a9c33c562a2dd31221730a8442e10422ca19
Valentina Ovsianik <forovp@gmail.com>
2024-10-05 13:30:26 +0300

    Реализована форма обратной связи на странице контакто

--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
 def home(request):
     return render(request, 'home.html')
 
 def contacts(request):
+    if request.method == 'POST':
+        # Получение данных из формы
+        name = request.POST.get('name')
+        message = request.POST.get('message')
+        return HttpResponse(f"Спасибо, {name}! Ваше сообщение получено.")
     return render(request, 'contacts.html')
