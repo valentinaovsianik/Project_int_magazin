99ab265d4e7017a3dddb496985aeca6ab7e03fea
Valentina Ovsianik <forovp@gmail.com>
2024-10-23 20:57:39 +0300

    Применены миграции

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,16 @@
 from django.db import models
 
 class BlogPost(models.Model):
-    title = models.CharField(max_length=200)
-    content = models.TextField()
-    preview_image = models.ImageField(upload_to='blog_previews/')
-    created_at = models.DateTimeField(auto_now_add=True)
-    published = models.BooleanField(default=False)
-    views = models.PositiveIntegerField(default=0)
+    title = models.CharField(max_length=200, verbose_name="Заголовок")
+    content = models.TextField(verbose_name="Содержимое")
+    preview_image = models.ImageField(upload_to='blog_previews/', verbose_name="Превью изображение")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    published = models.BooleanField(default=False, verbose_name="Опубликовано")
+    views = models.PositiveIntegerField(default=0, verbose_name="Количество просмотров")
+
+    class Meta:
+        verbose_name = "Блоговая запись"
+        verbose_name_plural = "Блоговые записи"
 
     def __str__(self):
         return self.title
