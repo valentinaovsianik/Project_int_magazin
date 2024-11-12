258777274c0e79f582545fc585e4c5970f92f397
Valentina Ovsianik <forovp@gmail.com>
2024-11-12 21:22:06 +0300

    Созданы представления для управления регистрацией, входом и выходом пользователей

--- users/views.py
+++ users/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.views.generic import FormView
+from django.core.mail import send_mail
+from django.urls import reverse_lazy
+from .forms import UserRegistrationForm, UserLoginForm
+from django.conf import settings
+from django.contrib.auth.views import LoginView, LogoutView
 
-# Create your views here.
+class RegisterView(FormView):
+    template_name = "users/register.html"
+    form_class = UserRegistrationForm
+    success_url = reverse_lazy("catalog:product_list")
+
+    def form_valid(self, form):
+        user = form.save()
+        user.save()
+        login(self.request, user)
+
+        # Отправка приветственного письма
+        send_mail(
+            subject="Добро пожаловать!",
+            message="Спасибо за регистрацию! Добро пожаловать в наш сервис!",
+            from_email=settings.DEFAULT_FROM_EMAIL,
+            recipient_list=[user.email],
+            fail_silently=False,
+        )
+
+        return super().form_valid(form)
+
+class UserLoginView(LoginView):
+    authentication_form = UserLoginForm
+    template_name = "users/login.html"
+    success_url = reverse_lazy("catalog:product_list")
+
+
+class UserLogoutView(LogoutView):
+    next_page = reverse_lazy("catalog:home")
