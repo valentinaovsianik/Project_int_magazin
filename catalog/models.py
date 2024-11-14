afe856cb234918c33f72e65a465176f72ddbe76c
Valentina Ovsianik <forovp@gmail.com>
2024-11-14 22:15:43 +0300

    К модели продукта добавлено поле is_published и кастомные права

--- catalog/models.py
+++ catalog/models.py
@@ -48,11 +48,16 @@ class Product(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата последнего изменения")
     is_active = models.BooleanField(default=True, verbose_name="Активный продукт")
+    is_published = models.BooleanField(default=False, verbose_name="Опубликован")
+
 
     class Meta:
         verbose_name = "Продукт"
         verbose_name_plural = "Продукты"
         ordering = ["name", "category", "price", "created_at", "updated_at"]
+        permissions = [
+            ("can_unpublish_product", "Can unpublish product")
+        ]
 
     def __str__(self):
         return self.name
