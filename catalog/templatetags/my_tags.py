23d213702de70cf8fd4a20f9cd9b76151304f71e
Valentina Ovsianik <forovp@gmail.com>
2024-11-18 23:55:46 +0300

    Добавлен декоратор

--- catalog/templatetags/my_tags.py
+++ catalog/templatetags/my_tags.py
@@ -2,9 +2,13 @@ from django import template
 
 register = template.Library()
 
-
 @register.filter()
 def media_filter(path):
     if path:
         return f"/media/{path}"
     return "#"
+
+
+@register.filter
+def is_moderator(user):
+    return user.groups.filter(name="Moderator Products").exists()
